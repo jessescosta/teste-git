348fdf680ecdee1f9981c8f66ba02929819bf754
jessescosta <jesse.dossantoscosta@gmail.com>
2023-06-28 16:12:46 -0300

    Implementando funcionalidade

--- Teste Git/src/Main.java
+++ Teste Git/src/Main.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+        nome = scanner.nextLine();
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("i = " + i +" " +nome);
         }
     }
 }
